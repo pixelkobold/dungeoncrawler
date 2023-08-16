426198e23684b99f05245baca1ddcf0781d1afd3
lunarW1TCH <zegnalekadrian@outlook.com>
2023-08-16 10:46:57 +0200

    feat: created basic functionality on old folder structure

--- core/src/main/java/com/pixelkobold/world/Worlds.java
+++ core/src/main/java/com/pixelkobold/world/Worlds.java
@@ -2,7 +2,9 @@ package com.pixelkobold.world;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
@@ -50,40 +52,48 @@ public class Worlds {
     }
 
     public static Floor generateFloor(Integer roomsAmount) {
-
-        // TODO: read all files, from folder, no need to use World class
-
         Floor floor = new Floor();
 
-        World entrance = getRandomWorld(WorldType.ENTRANCE);
-        World boss = getRandomWorld(WorldType.BOSS);
-        Array<World> regulars = new Array<>();
+        // TODO: fix it
+        // TODO: merge with new maps structure
+        Object[] files = Arrays.stream(Gdx.files.internal("maps/petri/").readString().split("\n")).filter(file -> file.endsWith("tmx")).toArray();
+        Array<String> maps = new Array<>();
 
-        for (int i = 0; i < roomsAmount; i++) {
-            regulars.add(getRandomWorld(WorldType.REGULAR));
+        for (Object file : files) {
+            maps.add(file.toString());
         }
 
+        // TODO: choose maps randomly instead of hardcoding indexes
+        String entrance = maps.get(0);
+        String boss = maps.get(0);
+        Array<String> regulars = new Array<>();
+
+        for (int i = 0; i < roomsAmount; i++) {
+            regulars.add(maps.get(i));
+        }
 
         // TODO: read properties of maps and add side rooms
 
-        Room entranceRoom = new Room(entrance.mapName, regulars.items[0].mapName, null, null, null);
+        Room entranceRoom = new Room(entrance, regulars.get(0), null, null, null);
         floor.addRoom(entranceRoom);
 
         for (int i = 0; i < roomsAmount; i++) {
             if (i == 0) {
-                Room room = new Room(regulars.items[i].mapName, entrance.mapName, null, null, null);
+                Room room = new Room(regulars.get(i), null, entrance, null, null);
                 floor.addRoom(room);
             } else if (i == roomsAmount - 1) {
-                Room room = new Room(regulars.items[i].mapName, null, boss.mapName, null, null);
+                Room room = new Room(regulars.get(i), boss, null, null, null);
                 floor.addRoom(room);
             } else {
-                Room room = new Room(regulars.items[i].mapName, regulars.items[i + 1].mapName, regulars.items[i - 1].mapName, null, null);
+                Room room = new Room(regulars.get(i), regulars.get(i + 1), regulars.get(i - 1), null, null);
                 floor.addRoom(room);
             }
         }
 
-        Room bossRoom = new Room(boss.mapName, null, regulars.items[roomsAmount - 1].mapName, null, null);
+        Room bossRoom = new Room(boss, null, regulars.get(roomsAmount - 1), null, null);
         floor.addRoom(bossRoom);
+
+        Log.log(LogLevel.DEBUG, floor.toString());
 
         return floor;
     }
