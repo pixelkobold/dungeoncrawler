2dd4e07286f151820c3f230da221341ca503a918
lunarW1TCH <zegnalekadrian@outlook.com>
2023-08-19 18:23:17 +0200

    feat: added a set of basic maps to levels: forest, tavern, frost and jungle

--- assets/maps/petri/sewer/SewerBlue_Tiled.tsx
+++ assets/maps/petri/sewer/SewerBlue_Tiled.tsx
@@ -1,4 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="SewerBlue_Tiled" tilewidth="16" tileheight="16" tilecount="77" columns="11">
- <image source="SewerBlue_PNG.png" width="176" height="112"/>
+<tileset version="1.10" tiledversion="1.10.2" name="SewerBlue_Tiled" tilewidth="16" tileheight="16" tilecount="99" columns="11">
+ <image source="SewerBlue_PNG.png" width="176" height="144"/>
+ <wangsets>
+  <wangset name="floor_sewer" type="mixed" tile="-1">
+   <wangcolor name="sewer" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="floor" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="5" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="46" wangid="2,2,2,2,2,1,1,1"/>
+   <wangtile tileid="47" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="48" wangid="2,1,1,1,2,2,2,2"/>
+   <wangtile tileid="49" wangid="2,2,1,1,2,2,2,2"/>
+   <wangtile tileid="50" wangid="2,2,2,2,2,1,1,2"/>
+   <wangtile tileid="56" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="57" wangid="2,2,1,1,1,1,1,1"/>
+   <wangtile tileid="58" wangid="2,2,1,1,1,1,1,2"/>
+   <wangtile tileid="59" wangid="2,1,1,1,1,1,1,2"/>
+   <wangtile tileid="60" wangid="2,2,1,1,1,1,1,2"/>
+   <wangtile tileid="61" wangid="2,2,1,1,1,1,1,2"/>
+   <wangtile tileid="63" wangid="2,2,1,1,1,1,1,2"/>
+   <wangtile tileid="70" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="71" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="88" wangid="1,1,1,2,2,1,1,1"/>
+   <wangtile tileid="89" wangid="1,1,1,2,2,2,1,1"/>
+   <wangtile tileid="90" wangid="1,1,1,1,2,2,1,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
